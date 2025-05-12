91917bad16e7cdf06b3a5d3d9c6b3e54f0ca7036
ruchita <ruchitapawar4@gmail.com>
2025-05-12 20:49:45 +0530

    error

--- app/meals/share/error.js
+++ app/meals/share/error.js
@@ -1,10 +1,11 @@
 "use client";
 
-export default function Error() {
+export default function Error({error}) {
     return (
         <main className="error">
             <h1>An error occured</h1>
             <p>While creating data. Please try again</p>
+            <p>{error}</p>
         </main>
     );
 }
